c5520873e401995f28d595c26723659e7233c8d6
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-07 17:32:46 +0100

    removed file

--- scroll.js
+++
@@ -1,4 +0,0 @@
-// window.addEventListener('scroll', (event) => {
-//   document.body.style.backgroundColor =
-//     window.scrollY > 700 ? 'var(--color-background)' : 'var(--color-second-bg)';
-// });
