84c62fc024e856decc939953a83a56b3ebff3431
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-07 17:32:40 +0100

    moved file

---
+++ js/scroll.js
@@ -0,0 +1,4 @@
+// window.addEventListener('scroll', (event) => {
+//   document.body.style.backgroundColor =
+//     window.scrollY > 700 ? 'var(--color-background)' : 'var(--color-second-bg)';
+// });
