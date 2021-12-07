4fa2cd0d95be0949946be9719a4afcedc4139e83
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-07 19:39:33 +0100

    Updated if-statement

--- js/click.js
+++ js/click.js
@@ -5,7 +5,9 @@ document.querySelector('.powerButton').addEventListener('click', function () {
   const imgBefore = document.querySelector('.christmastreeNotLight');
   if (imgBefore.src.match('/images/christmastreeNotLight.svg')) {
     imgBefore.src = '/images/halfLight.svg';
-  } else {
+  } else if (imgBefore.src.match('/images/halfLight.svg')) {
     imgBefore.src = '/images/christmastreeLight.svg';
+  } else {
+    imgBefore.src = '/images/christmastreeNotLight.svg';
   }
 });
