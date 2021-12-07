74b109bdf274e32af294e8fbb2e0e39cb315e79e
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-07 17:31:42 +0100

    moved file

--- doubleClick.js
+++
@@ -1,9 +0,0 @@
-function songDblclick() {
-  const christmasSong = document.querySelector('.mySound');
-  const powerButton = document.querySelector('.powerButton');
-  if (!christmasSong) return;
-  christmasSong.currentTime = 0;
-  christmasSong.play();
-  powerButton.classList.add('doubleclick');
-}
-window.addEventListener('dblclick', songDblclick);
