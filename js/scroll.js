818c665ecee7c8e2d87f850ad6cb9c86c860587b
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-09 13:45:57 +0100

    Added descriptive text to scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,8 @@
-//
+// funcion that changes the background color when user scroll down.
+// Creates a new class "BackgroundColor".
+
 function changeColor() {
-  if (window.scrollY > window.innerHeight / 15) {
+  if (window.scrollY > window.innerHeight / 10) {
     document.body.classList.add('backgroundColor');
   } else {
     document.body.classList.remove('backgroundColor');
