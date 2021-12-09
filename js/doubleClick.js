54e571f0e6f46abf178b14da8132882c8231955b
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-09 07:00:38 +0100

    updated dblclick event listener with new button

--- js/doubleClick.js
+++ js/doubleClick.js
@@ -1,15 +1,14 @@
 // Function that connects audio with button and play and
 // pause music with doubleclick
 
-document
-  .querySelector('.powerButton')
-  .addEventListener('dblclick', function () {
-    const christmasSong = document.querySelector('.mySound');
-    const powerButton = document.querySelector('.powerButton');
-    if (christmasSong.paused) {
-      christmasSong.loop = true;
-      christmasSong.play();
-    } else {
-      christmasSong.pause();
-    }
-  });
+document.querySelector('.bell').addEventListener('dblclick', function () {
+  const christmasSong = document.querySelector('.mySound');
+  document.querySelector('.bell').classList.add('moveBell');
+  if (christmasSong.paused) {
+    christmasSong.loop = true;
+    christmasSong.play();
+  } else {
+    christmasSong.pause();
+    document.querySelector('.bell').classList.remove('moveBell');
+  }
+});
